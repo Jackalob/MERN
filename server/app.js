fbef370ea016728ce8aee049c3bfbf5d4aa1db76
Jackalob <nike9712@gmail.com>
2023-05-15 16:49:02 +0800

    feat: add express router

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Hi, there' });
-});
+app.use('/api/workouts', workoutRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log('listening on port', process.env.PORT);
