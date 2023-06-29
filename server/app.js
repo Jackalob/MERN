850288750e2d79829faa4e9704f281bebc07b737
Jackalob <nike9712@gmail.com>
2023-06-29 17:07:11 +0800

    feat: add connection to db

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 const app = express();
@@ -14,6 +15,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to the db & listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
